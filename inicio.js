68b0da3b8c4a86b45f1276e8a2f35b7043f18002
fefucho08 <63268454+fefucho08@users.noreply.github.com>
2023-01-17 01:34:09 -0300

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,15 @@
+function valida(){
+    var senha= input_senha.value;
+
+    if(senha=="Destinada"){
+        window.location="desktop/desktop.html";
+    }else{
+        alert("Senha incorreta")
+    }
+    
+    
+}
+
+function dica(){
+    alert("Dica de Segurança: Nome do seu livro favorito")
+}
