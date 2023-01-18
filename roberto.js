fee787350c43076613120ab84881b5ce91d462df
fefucho08 <63268454+fefucho08@users.noreply.github.com>
2023-01-18 13:42:45 -0300

    Add files via upload

---
+++ roberto.js
@@ -0,0 +1,9 @@
+function valida(){
+    var senha= input_senha.value;
+
+    if(senha=="Sangue"){
+        window.location="desktop2/pre.html";
+    }else{
+        alert("Senha incorreta")
+    }
+}
